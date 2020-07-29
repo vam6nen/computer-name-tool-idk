0303bd28986d4a32940d3c9a4b71005f731e4451
vam6nen <68958851+vam6nen@users.noreply.github.com>
2020-07-29 17:36:50 +0300

    Add files via upload

---
+++ cns 1 - ENG.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+title computer name spotter 1.0 by P.P - open source i dont give a shit about you stealing code lol
+mode con cols=90lines=5
+echo -----------------------
+echo computer name is
+echo "%computername%"!
+echo -----------------------
+pause >NUL
